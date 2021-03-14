c037d302c0e5b117f3a6840453a48881c482f931
Jeff.A <kinesthesiac@gmail.com>
2021-03-13 21:19:37 -0800

    basic files and npm

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk")
+const chalkAnimation = require("chalk-animation")
+const figlet = require("figlet")
+const inquirer = require("inquirer")
+
+console.log(figlet.textSync('Text Game'))
+chalkAnimation.rainbow("Welcome to this text game!")
